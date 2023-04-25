15aedbfd5ed1d022e5ce9fa7025c42ca17c581a8
mihajlo <122311433+oljahim@users.noreply.github.com>
2023-04-25 22:27:44 +0200

    Added pokemonList array with 3 Pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-alert("Hello world");
+let pokemonList = [];
 
-let favoriteFood = "Apple";
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 7,
+  types: ["grass", "poison"],
+};
 
-document.write(favoriteFood);
+pokemonList[1] = {
+  name: "Charmander",
+  height: 6,
+  types: ["fire"],
+};
+
+pokemonList[2] = {
+  name: "Squirtle",
+  height: 5,
+  types: ["water"],
+};
