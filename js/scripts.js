606464aa5d8a05dec711deed242b3ec452ea9850
mihajlo <122311433+oljahim@users.noreply.github.com>
2023-04-26 02:44:31 +0200

    Add loop to display pokemon list with their sizes

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,33 @@ let pokemonList = [];
 
 pokemonList[0] = {
   name: "Bulbasaur",
-  height: 7,
+  height: 0.7,
   types: ["grass", "poison"],
 };
 
 pokemonList[1] = {
   name: "Charmander",
-  height: 6,
+  height: 0.6,
   types: ["fire"],
 };
 
 pokemonList[2] = {
   name: "Squirtle",
-  height: 5,
+  height: 0.5,
   types: ["water"],
 };
+
+// Loops through the pokemonList
+// Checks if height is above a certain value
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemonHeight = pokemonList[i]["height"];
+  let pokemonName = pokemonList[i]["name"];
+
+  if (pokemonHeight > 0.6) {
+    document.write(
+      `${pokemonName} (height: ${pokemonHeight}) - Wow, that’s big!\n`
+    );
+  } else {
+    document.write(`${pokemonName} (height: ${pokemonHeight})\n`);
+  }
+}
